fa0409417130f535209928bff379c028e59972ba
Mahmoud Mohamed <ma5975986@gmail.com>
2025-10-02 13:58:12 +0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/video_tube_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.video_tube_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:video_tube_app/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
