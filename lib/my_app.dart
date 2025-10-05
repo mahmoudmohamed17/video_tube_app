d8adde484b06ca1b774952b3f00dd44b61559443
Mahmoud Mohamed <ma5975986@gmail.com>
2025-10-05 21:23:13 +0300

    Modify logo and splash screen

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -6,10 +6,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      title: 'VideoTube',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
   }
